6223bf08bbe6175fe48dc7bce5f5b905c7b93f33
Harshith Guru Prasad <harshith100gp@gmail.com>
2016-10-17 12:29:07 -0700

    Update stack.java

--- stack.java
+++ stack.java
@@ -22,8 +22,8 @@ public class stack
     	//constructor to initialize the stack
     	stack(int size)
     	{
-    			Stack_Size = size;
-    			Stack_Array = new String[size];
+    		Stack_Size = size;
+    		Stack_Array = new String[size];
     	}	
     	
     	
@@ -31,68 +31,68 @@ public class stack
     	public void push(String input)
     	{
     		//check for stack overflow
-    			if(Top_Of_Stack+1 < Stack_Size)
-    			{
-    					Stack_Array[++Top_Of_Stack] = input;
-    					System.out.println("PUSH OPERATION : " + input + " was pushed onto the Stack");
-    					displayStack_Object();
-    			} 
+    		if(Top_Of_Stack+1 < Stack_Size)
+    		{
+    			Stack_Array[++Top_Of_Stack] = input;
+    			System.out.println("PUSH OPERATION : " + input + " was pushed onto the Stack");
+    			displayStack_Object();
+    		} 
     			
-    			else
-    				System.out.println("Stack Overflow: "+input+ " Cannot be Pushed");
+    		else
+    			System.out.println("Stack Overflow: "+input+ " Cannot be Pushed");
     	}
   
     	//POP operation
     	public  void pop()
     	{
-    			//check for stack underflow
-    			if(Top_Of_Stack < 0)
-    			{
-    					System.out.println("Stack UnderFlow: No item to be delected");
-    					displayStack_Object();
-    			}
-    			else 
-    			{
-    					System.out.println("POP OPERATION : " + Stack_Array[Top_Of_Stack--] + " was popped from the Stack");
-    					displayStack_Object();
-    			}
+    		//check for stack underflow
+    		if(Top_Of_Stack < 0)
+    		{
+    			System.out.println("Stack UnderFlow: No item to be delected");
+    			displayStack_Object();
+    		}
+    		else 
+    		{
+    			System.out.println("POP OPERATION : " + Stack_Array[Top_Of_Stack--] + " was popped from the Stack");
+    			displayStack_Object();
+    		}
     	}
     	
     	public void peek()
     	{
-    			if(Top_Of_Stack==-1)
-    				System.out.println("Stack Empty");
-    			else
-    					System.out.println("PEEK: " + Stack_Array[Top_Of_Stack] + " is at the Top of the Stack");
+    		if(Top_Of_Stack==-1)
+    			System.out.println("Stack Empty");
+    		else
+    			System.out.println("PEEK: " + Stack_Array[Top_Of_Stack] + " is at the Top of the Stack");
     	}
     	
     	// push multiple values
     	public void pushMany(String multipleValues)
     	{
-    			String[] tempString = multipleValues.split(" ");
-    			for(int i = 0; i < tempString.length; i++)
-    					push(tempString[i]);
+    		String[] tempString = multipleValues.split(" ");
+    		for(int i = 0; i < tempString.length; i++)
+    			push(tempString[i]);
     	}
     	
     	// pop all stack contents
     	public void popAll()
     	{
-    			if(Top_Of_Stack<0)
-    					System.out.println("Stack Empty");
-    			else
-    					for(int i = Top_Of_Stack; i >= 0; i--)	
-    							pop();
+    		if(Top_Of_Stack<0)
+    			System.out.println("Stack Empty");
+    		else
+    			for(int i = Top_Of_Stack; i >= 0; i--)	
+    				pop();
     	}
     	
     	public void displayStack_Object()
     	{
     		if(Top_Of_Stack==-1)
-    				System.out.println("Empty Stack");
+    			System.out.println("Empty Stack");
     		else
     		{
             		System.out.println("The elements of the stack: ");
             		for(int i=Top_Of_Stack;i>=0;i--)
-            				System.out.println(Stack_Array[i]);	
+            			System.out.println(Stack_Array[i]);	
             		for(int i=0;i<100;i++)
             			System.out.print("-");
             		System.out.println();
@@ -101,69 +101,69 @@ public class stack
     	
     	public static void main(String[] args)throws IOException
     	{
-    			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    			stack Stack_Object = new stack(10);
+    		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+    		stack Stack_Object = new stack(10);
     			
     			int x=0,ch=0;
     			while(x==0)
     			{
-    					System.out.println("----------Stack----------");
-    					System.out.println("1. Push");
-    					System.out.println("2. Push Many");
-    					System.out.println("3. Pop");
-    					System.out.println("4. Pop All");
-    					System.out.println("5. Peek");
-    					System.out.println("6. Display");
-    					System.out.println("0. Exit");
-    					System.out.println("Enter your choice of operation : ");
-    					String inp=br.readLine();
-    					try
-    					{
-    						//input validation
-    						ch=Integer.parseInt(inp);
-    						if(ch>=0 && ch<=6)
-    							x=1;
-    						else
-    							System.out.println("Incorrect Input Choice \n");
-    					}
-    					catch(NumberFormatException e)
-    					{
-    							System.out.println(e);
-    					}
+    				System.out.println("----------Stack----------");
+    				System.out.println("1. Push");
+    				System.out.println("2. Push Many");
+    				System.out.println("3. Pop");
+    				System.out.println("4. Pop All");
+    				System.out.println("5. Peek");
+    				System.out.println("6. Display");
+    				System.out.println("0. Exit");
+    				System.out.println("Enter your choice of operation : ");
+    				String inp=br.readLine();
+    				try
+    				{
+    					//input validation
+    					ch=Integer.parseInt(inp);
+    					if(ch>=0 && ch<=6)
+    						x=1;
+    					else
+    						System.out.println("Incorrect Input Choice \n");
+    				}
+    				catch(NumberFormatException e)
+    				{
+    					System.out.println(e);
+    				}
     					
-    					if(x==1)
+    				if(x==1)
+    				{
+    					x=0;
+    						
+    					switch(ch)
     					{
-    							x=0;
-    						
-    							switch(ch)
-    							{
-    									case 1: 
-    													System.out.println("Enter element to be pushed");
-    													inp=br.readLine();
-    													Stack_Object.push(inp);
-    													break;
+    						case 1: 
+    							System.out.println("Enter element to be pushed");
+    							inp=br.readLine();
+    							Stack_Object.push(inp);
+    							break;
     									
-    									case 2:   System.out.println("Enter the elements to be pushed");
-														inp=br.readLine();
-														Stack_Object.pushMany(inp);
-    													break;
+    						case 2: System.out.println("Enter the elements to be pushed");
+							inp=br.readLine();
+							Stack_Object.pushMany(inp);
+    							break;
     													
-    									case 3:  Stack_Object.pop(); 
-    													break;
+    						case 3: Stack_Object.pop(); 
+    						        break;
     													
-    									case 4:  Stack_Object.popAll();
-										                break;
+    						case 4: Stack_Object.popAll();
+							break;
 										
-    									case 5:  Stack_Object.peek();
-													    break;
+    						case 5: Stack_Object.peek();
+							break;
 													    
-    									case 6: Stack_Object.displayStack_Object(); break;
+    						case 6: Stack_Object.displayStack_Object(); break;
 													    
-    									case 0: System.out.println("Have a nice day ");
-    													System.exit(0);
-    						}// end of switch
+    						case 0: System.out.println("Have a nice day ");
+    							System.exit(0);
+    					}// end of switch
     						
-    					}//end of if
+    				}//end of if
     						
     			}//end of while
 
